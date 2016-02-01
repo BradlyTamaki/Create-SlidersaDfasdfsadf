5ee7494cabd71090d08ccb4d5019972dc4916cae
bradlytamaki <bradlytamaki@gmail.com>
2016-02-01 14:43:09 -0800

    init

---
+++ Create Slider/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="8319c7f4-133f-44e1-873b-933b2bdcda02" description="SharePoint App Feature" featureId="8319c7f4-133f-44e1-873b-933b2bdcda02" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="Create Slider Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="6eb8e437-8111-4a8b-9e14-8c0284db3b45" />
+    <projectItemReference itemId="3bf2017f-3e93-4dbe-96ec-962cb3ca29a0" />
+    <projectItemReference itemId="f13eeff2-4e92-4d47-bf66-73a8443634b7" />
+    <projectItemReference itemId="3bd7b2d1-965b-492f-a892-76e56abb1758" />
+  </projectItems>
+</feature>
---
+++ Create Slider/Scripts/App.js
@@ -0,0 +1,101 @@
+﻿'use strict';
+
+ExecuteOrDelayUntilScriptLoaded(initializePage, "sp.js");
+
+function initializePage()
+{
+    var context = SP.ClientContext.get_current();
+    var user = context.get_web().get_currentUser();
+
+    // This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model
+    $(document).ready(function () {
+        $('#siteContent').attr('href', decodeURIComponent(getQueryStringParameter('SPHostUrl')) + "/_layouts/15/viewlsts.aspx");
+        $('#createList').click(createList);
+    });
+
+    //#1 Create List
+    function createList() {
+        // Create an announcement SharePoint list with the name that the user specifies.
+        var hostUrl = decodeURIComponent(getQueryStringParameter("SPHostUrl"));
+        var currentcontext = new SP.ClientContext.get_current();
+        var hostcontext = new SP.AppContextSite(currentcontext, hostUrl);
+        var hostweb = hostcontext.get_web();
+
+        //Set ListCreationInfomation()
+        var listCreationInfo = new SP.ListCreationInformation();
+        listCreationInfo.set_title('Sliderr');
+        listCreationInfo.set_templateType(SP.ListTemplateType.genericList);
+        var newList = hostweb.get_lists().add(listCreationInfo);
+
+        //Set column data
+        var newCols = [
+            "<Field Type='Note' DisplayName='Body' Required='FALSE' EnforceUniqueValues='FALSE' NumLines='6' RichText='TRUE' RichTextMode='FullHtml' StaticName='Body' Name='Body'/>",
+            "<Field Type='URL' DisplayName='Background Image' Required='FALSE' EnforceUniqueValues='FALSE' Format='Hyperlink' StaticName='BackgroundImage' Name='BackgroundImage'/>",
+            "<Field Type='Boolean' DisplayName='Enabled' EnforceUniqueValues='FALSE' StaticName='Enabled' Name='Enabled'><Default>1</Default></Field>",
+            "<Field Type='DateTime' DisplayName='Expire' Required='FALSE' EnforceUniqueValues='FALSE' Format='DateTime' FriendlyDisplayFormat='Disabled' StaticName='Expire' Name='Expire'/>",
+            "<Field Type='Number' DisplayName='Order' Required='FALSE' EnforceUniqueValues='FALSE' StaticName='Order0' Name='Order0'/>"
+        ];
+        var newListWithColumns;
+        for (var i = 0; i < newCols.length; i++) {
+            newListWithColumns = newList.get_fields().addFieldAsXml(newCols[i], true, SP.AddFieldOptions.defaultValue);
+        }
+
+        //final load/execute
+        context.load(newListWithColumns);
+        context.executeQueryAsync(function () {
+            console.log('List created successfully!');
+            uploadTileImage()
+        },
+        function (sender, args) {
+            console.error(sender);
+            console.error(args);
+            alert('Failed to create the list. ' + args.get_message());
+        });
+    }
+
+    //#2 Upload Tile Image
+    function uploadTileImage() {
+        BinaryUpload.Uploader().Upload("/images/l_Sliderx96.png", "/SiteAssets/l_Sliderx96.png");
+        setImageUrl();
+    }
+
+    //#3 Set Tile Image
+    function setImageUrl() {
+        var urlTarget = decodeURIComponent(getQueryStringParameter('SPAppWebUrl') + '/_api/SP.AppContextSite(@target)/web/lists/getByTitle(\'Sliderr\')?@target=\'' + getQueryStringParameter('SPHostUrl') + '\'');
+        var imgTarget = decodeURIComponent(getQueryStringParameter('SPHostUrl')) + '/SiteAssets/l_Sliderx96.png'
+        console.log(urlTarget);
+        $.ajax({
+            method: 'POST',
+            url: urlTarget,
+            data: JSON.stringify({ '__metadata': { 'type': 'SP.List' }, 'ImageUrl': imgTarget }),
+            contentType: 'application/json;odata=verbose',
+            headers: {
+                'accept': 'application/json;odata=verbose',
+                'X-RequestDigest': $('#__REQUESTDIGEST').val(),
+                'X-HTTP-Method': 'MERGE',
+                'If-Match': '*'
+            },
+            success: function (data) {
+                console.log('successfully set ImageUrl');
+                $('#sliderListContainer').show();
+                alert('Slider list created successfully! Click on the Slider List to continue.');
+            },
+            error: function (err) {
+                console.error(err);
+                alert('Failed to set List.ImageUrl. Please notifiy your SharePoint Admin.');
+            }
+        });
+    }
+
+}
+
+function getQueryStringParameter(param) {
+    var params = document.URL.split("?")[1].split("&");
+    var strParams = "";
+    for (var i = 0; i < params.length; i = i + 1) {
+        var singleParam = params[i].split("=");
+        if (singleParam[0] == param) {
+            return singleParam[1];
+        }
+    }
+}
